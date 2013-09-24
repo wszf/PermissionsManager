458c77ac8e6de8fc5fd9c22f9382c72796210a58
wszfer <wszfer@gmail.com>
2013-09-25 03:20:39 +0800

    init

---
+++ src/com/mokee/permissionsmanager/activites/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mokee.permissionsmanager.activites;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mokee.permissionsmanager.R;
+
+public class MainActivity extends Activity
+	{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+		{
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.test_main);
+		}
+
+	}
---
+++ src/com/mokee/permissionsmanager/common/Utils.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2007 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.mokee.permissionsmanager.common;
+
+import android.content.res.Resources;
+import android.preference.PreferenceFrameLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TabWidget;
+
+public class Utils {
+
+    private static final String TAG = "Utils";
+
+    /**
+     * Set the preference's title to the matching activity's label.
+     */
+    public static final int UPDATE_PREFERENCE_FLAG_SET_TITLE_TO_MATCHING_ACTIVITY = 1;
+
+    /**
+     * The opacity level of a disabled icon.
+     */
+    public static final float DISABLED_ALPHA = 0.4f;
+
+    /**
+     * Name of the meta-data item that should be set in the AndroidManifest.xml
+     * to specify the icon that should be displayed for the preference.
+     */
+    private static final String META_DATA_PREFERENCE_ICON = "com.android.settings.icon";
+
+    /**
+     * Name of the meta-data item that should be set in the AndroidManifest.xml
+     * to specify the title that should be displayed for the preference.
+     */
+    private static final String META_DATA_PREFERENCE_TITLE = "com.android.settings.title";
+
+    /**
+     * Name of the meta-data item that should be set in the AndroidManifest.xml
+     * to specify the summary text that should be displayed for the preference.
+     */
+    private static final String META_DATA_PREFERENCE_SUMMARY = "com.android.settings.summary";
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
+    // Device type reference
+    private static int mDeviceType = -1;
+
+  
+
+    public static void forcePrepareCustomPreferencesList(
+            ViewGroup parent, View child, ListView list, boolean ignoreSidePadding) {
+        list.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        list.setClipToPadding(false);
+        prepareCustomPreferencesList(parent, child, list, ignoreSidePadding);
+    }
+
+    /**
+     * Prepare a custom preferences layout, moving padding to {@link ListView}
+     * when outside scrollbars are requested. Usually used to display
+     * {@link ListView} and {@link TabWidget} with correct padding.
+     */
+    public static void prepareCustomPreferencesList(
+            ViewGroup parent, View child, View list, boolean ignoreSidePadding) {
+        final boolean movePadding = list.getScrollBarStyle() == View.SCROLLBARS_OUTSIDE_OVERLAY;
+        if (movePadding && parent instanceof PreferenceFrameLayout) {
+            ((PreferenceFrameLayout.LayoutParams) child.getLayoutParams()).removeBorders = true;
+
+            final Resources res = list.getResources();
+            final int paddingSide = res.getDimensionPixelSize(
+                    com.android.internal.R.dimen.preference_fragment_padding_side);
+            final int paddingBottom = res.getDimensionPixelSize(
+                    com.android.internal.R.dimen.preference_fragment_padding_bottom);
+
+            final int effectivePaddingSide = ignoreSidePadding ? 0 : paddingSide;
+            list.setPaddingRelative(effectivePaddingSide, 0, effectivePaddingSide, paddingBottom);
+        }
+    }
+
+   
+}
